5dba15582bcd2d1c38d15428e3da8a35e6b71bf2
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-31 22:13:58 +0800

    sets up devise user and admin

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user do
+    full_name { Faker::Name.name }
+    age { Faker::Number.between(from: 1, to: 99) }
+    gender { Faker::Gender.binary_type }
+    dob { Faker::Date.birthday(min_age: 1, max_age: 99) }
+    contact_no { Faker::PhoneNumber.phone_number_with_country_code }
+    email { Faker::Internet.email }
+    address { Faker::Address.full_address }
+
+    trait :admin do
+      admin { true }
+    end
+
+    trait :confirmed do
+      confirmed_at { 1.day.ago }
+    end
+  end
+end
