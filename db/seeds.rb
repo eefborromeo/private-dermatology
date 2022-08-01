f7707124ebd7270e430a3d3a3301d3fb671f6767
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-01 19:16:57 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(
+    email: 'admin@privatedermatology.com',
+    password: 'admin123',
+    password_confirmation: "admin123",
+    full_name: 'Admin',
+    gender: nil,
+    dob: nil, 
+    contact_no: nil,
+    address: nil,
+    admin: true,
+)
+
+admin.skip_confirmation!
+admin.save
+
+patient = User.create(
+    email: 'patient@privatedermatology.com',
+    password: 'patient123',
+    password_confirmation: "patient123",
+    full_name: 'Patient User',
+    gender: 'Female',
+    dob: '2022-08-01 00:00:00', 
+    contact_no: '+63 123 456 7890',
+    address: 'Cebu City, Cebu',
+    admin: false,
+)
+
+patient.skip_confirmation!
+patient.save
