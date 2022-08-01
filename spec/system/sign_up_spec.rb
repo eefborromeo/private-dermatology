ccae3115f1fc1fad700f3eb9ba2369ee198695ab
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-01 11:14:53 +0800

    update system spec for sign up

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe "Sign up", type: :system do
         fill_in "Full name", with: user.full_name
         select user.gender, from: "Gender"
         fill_in "Date of Birth", with: user.dob
-        fill_in "Contact No", with: user.contact_no
-        fill_in "Email", with: user.email
+        fill_in "Contact No.", with: user.contact_no
         fill_in "Address", with: user.address
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        fill_in "Password confirmation", with: user.password
         click_button "Sign up"
-        expect(current_path).to eq(new_user_session_path)
+        expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
     end
 end
