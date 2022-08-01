38015c3f51f3dd41f928c1f579df539e313b162c
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-01 20:59:26 +0800

    add controller and views for add product to inventory

--- app/controllers/admin/inventory_controller.rb
+++ app/controllers/admin/inventory_controller.rb
@@ -1,2 +1,31 @@
 class Admin::InventoryController < ApplicationController
+    before_action :is_admin
+
+    def index
+
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to admin_inventory_index_path, notice: "Product added to inventory"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def is_admin
+        redirect_to root_path unless current_user.admin?
+    end
+
+    def product_params
+        params.require(:product).permit(:product_name, :product_desc, :price, :stock)
+    end
 end
