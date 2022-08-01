e68d5e274a23532cbba8050a79203c6b391c9654
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-01 19:14:07 +0800

    add tests for mailer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:user) { create(:user) }
+
+  describe "after creation" do
+    it "sends a confirmation email" do
+      new_user = build(:user)
+      expect { new_user.save }.to change(Devise.mailer.deliveries, :count).by(1)
+    end
+  end
 end
