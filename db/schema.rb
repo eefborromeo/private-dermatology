eb5fc9d0b065d0230c10f7cc25eb038d9faea96d
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-02 21:21:34 +0800

    generate cart item model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_121849) do
+ActiveRecord::Schema.define(version: 2022_08_02_131727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["user_id"], name: "index_cart_items_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "product_name"
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2022_08_01_121849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "users"
 end
