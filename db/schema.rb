388e25e541470c82c8d9380d9b09526cb8040093
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-08-05 07:05:53 +0800

    add appointment slots table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_083903) do
+ActiveRecord::Schema.define(version: 2022_08_04_230520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2022_08_04_083903) do
     t.text "product_desc"
     t.integer "price"
     t.integer "stocks"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "slots", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean "availability"
+    t.integer "interaction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
