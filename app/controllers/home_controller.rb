1c6215ae61e74114a9418859e4d5bd718a0fec61
lkpallarca <Ckencepallarca@gmail.com>
2022-08-04 19:34:15 +0800

    add check user info before action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     skip_before_action :authenticate_user!
-    
-    def index
+    before_action :check_user_info, if: :user_signed_in?
         
+    def index
     end
 end
