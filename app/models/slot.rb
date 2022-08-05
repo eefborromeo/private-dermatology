2dae5cc17db56d13104841ccacb5b161e2a51d13
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-08-06 03:34:42 +0800

    add enum for interaction

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,7 +1,7 @@
 class Slot < ApplicationRecord
-  enum interaction: [:online, :face_to_face]
-  after_initialize :set_default_role, :if => :new_record?
-  def set_default_role
-    self.role ||= :online
+  enum interaction: [:online, :face_to_face, :both]
+  after_initialize :set_default_interaction, :if => :new_record?
+  def set_default_interaction
+    self.interaction ||= :both
   end
 end
