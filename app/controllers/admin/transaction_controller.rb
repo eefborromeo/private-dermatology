62a2746b96f776c7386f2c4afbef82bcf48f95ab
lkpallarca <Ckencepallarca@gmail.com>
2022-08-05 21:04:19 +0800

    create admin transaction controller

--- app/controllers/admin/transaction_controller.rb
+++ app/controllers/admin/transaction_controller.rb
@@ -1,10 +1,15 @@
 class Admin::TransactionController < ApplicationController
   def index
+    @transactions = Transaction.all
   end
 
   def show
+    @transaction = Transaction.find(params[:id])
   end
 
   def destroy
+    transaction = Transaction.find(params[:id])
+    transaction.destroy!
+    redirect_to admin_transaction_index_path, notice: "Successfully deleted transaction"
   end
 end
