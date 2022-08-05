b3196f61ee70065b24b2cdab2fcfaa818d609d37
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-08-06 03:59:21 +0800

    add enum for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :slot
   belongs_to :user
+
+  enum interaction: [:online, :face_to_face]
+  after_initialize :set_default_interaction, :if => :new_record?
+  def set_default_interaction
+    self.interaction ||= :online
 end
