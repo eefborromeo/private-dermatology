feaea0daf1cbf3f736b446d6587adec88c2ca0d5
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-06 09:50:27 +0800

    add image validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,21 @@ class Product < ApplicationRecord
     has_many :cart_items
     has_many :users, through: :cart_items
     has_one_attached :product_image
+
+    validate :acceptable_image
+
+    def acceptable_image
+        return unless product_image.attached?
+
+        unless product_image.byte_size <= 2.megabyte
+            errors.add(:product_image, "is too big")
+        end
+
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(product_image.content_type)
+            errors.add(:product_image, "must be a JPEG or PNG")
+        end
+    end
+
+    
 end
