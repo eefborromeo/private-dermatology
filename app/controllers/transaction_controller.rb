30ceaf56b97a90e6717d7e620eef3602805e9481
lkpallarca <Ckencepallarca@gmail.com>
2022-08-09 02:37:11 +0800

    integrate paymongo/gcash logic with transaction actions

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,9 +1,10 @@
 class TransactionController < ApplicationController
-  include TransactionHelper
   before_action :check_user_info, if: :user_signed_in?
+  include TransactionHelper
 
   def index
-    @transactions = current_user.transactions
+    @product_transactions = current_user.product_transactions
+    @appointment_transactions = current_user.appointment_transactions
   end
 
   def show
@@ -13,21 +14,20 @@ class TransactionController < ApplicationController
     if params[:i].nil? || params[:i].length == 0
       redirect_to cart_index_path, alert: "Please select items"
     else
-      @data = display_cart_items_to_checkout(params[:i])
+      checkout = display_cart_items_to_checkout(params[:i])
+      @data = checkout[0]
+      session[:transaction_data] = checkout[0]
+      @checkout_total = checkout[1]
     end
   end
 
-  def create
-    if params[:data].nil? || params[:data].length == 0
-      redirect_to root_path, alert: "Something went wrong, please try again."
+  def complete
+    if session[:payment_category] == "Dermatology Product Payment"
+      process_product_transaction(session[:transaction_data], session[:gcash_transaction_id])
+      redirect_to root_path, notice: "Your Product payment was successful!"
     else
-      process = process_transaction(params[:data])
-    end
-    
-    if process == true
-      redirect_to products_path, notice: "Succ"
-    else
-      redirect_to cart_index_path, alert: "Failure"
+      process_appointment_transaction(session[:transaction_data], session[:gcash_transaction_id])
+      redirect_to root_path, notice: "Your Appointment payment was successful!"
     end
   end
 
