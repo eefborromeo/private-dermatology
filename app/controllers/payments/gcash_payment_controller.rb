4109813cc620d0d946c3ebeff2c0a3747c391a27
lkpallarca <Ckencepallarca@gmail.com>
2022-08-10 14:56:50 +0800

    create product and appointment availability checks right before payment process

--- app/controllers/payments/gcash_payment_controller.rb
+++ app/controllers/payments/gcash_payment_controller.rb
@@ -1,15 +1,22 @@
 class Payments::GcashPaymentController < ApplicationController
+  include Payments::GcashPaymentHelper
+
   def create
     if params[:appointment_id]
       session[:transaction_data] = params[:appointment_id]
-    end
 
-    response = Payment::GcashPayment.initialize_source(params[:amount], params[:payment_category])
-    session[:gcash_source_id] = response[:source_id]
-    session[:payment_category] = response[:payment_category]
-    session[:transaction_amount] = response[:amount]
-    
-    redirect_to response[:source_checkout_url]
+      if check_slot_availability_before_payment(params[:appointment_id])
+        _process(cart_index_path)
+      else
+        redirect_to root_path, alert: "Something went wrong. Please try again"
+      end
+    else
+      if check_product_stock_availability_before_payment(session[:transaction_data])
+        _process(appointment_index_path)
+      else
+        redirect_to root_path, alert: "Something went wrong. Please try again"
+      end
+    end
   end
 
   def success    
@@ -22,5 +29,21 @@ class Payments::GcashPaymentController < ApplicationController
     session.delete(:transaction_amount)
     session.delete(:transaction_data)
     redirect_to root_path, alert: "Your transaction failed, please try again."
+  end
+
+  private
+
+  def _process(path_fallback)
+    if params[:amount].to_i < 100
+      redirect_to path_fallback, alert: "GCash payments only allow transactions more than ₱100." 
+      return
+    end
+
+    response = Payment::GcashPayment.initialize_source(params[:amount], params[:payment_category])
+    session[:gcash_source_id] = response[:source_id]
+    session[:payment_category] = response[:payment_category]
+    session[:transaction_amount] = response[:amount]
+    
+    redirect_to response[:source_checkout_url]
   end
 end
