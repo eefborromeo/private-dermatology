14b968dedd2facd06541bd47b08ec29ed150c38b
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-11 11:54:02 +0800

    add image to product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
     product_desc { "Acne Cream is used to treat acne" }
     price { 200.0 }
     stocks { 5 }
+    after(:build) do |product|
+      product.product_image.attach(io: File.open(Rails.root.join('spec', 'support', 'acnecream.png')), filename: 'acnecream.png', content_type: 'image/png')
+    end
   end
 end
