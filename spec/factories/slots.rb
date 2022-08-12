0f15a128b860dee0b1daa681e32db677914593ec
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-08-12 11:53:40 +0800

    creates testing and validations for slots

--- spec/factories/slots.rb
+++ spec/factories/slots.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :slot do
-    date { "2022-08-05 07:05:20" }
+    date { "2022-08-05" }
+    time { "07:05:20" }
     availability { false }
     interaction { 1 }
   end
